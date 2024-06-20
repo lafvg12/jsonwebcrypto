02a36826c7e7e25878e6ded8297de7395eb728c8
lafvg12 <lafvg12@hotmail.com>
2024-06-20 14:52:03 -0500

    Delete file test.ts

--- test.ts
+++
@@ -1,7 +0,0 @@
-import { derivePasswordHash, generateSalt } from "./src";
-
-const password = "secret key";
-const salt = generateSalt(16);
-
-const passwordhasehd = derivePasswordHash(password, salt);
-console.log("🚀 ~ passwordhasehd:", passwordhasehd);
